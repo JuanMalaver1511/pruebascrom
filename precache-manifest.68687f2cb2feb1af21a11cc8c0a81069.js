5a2e3c3d7b5368207ccc25ec5bf3da56141b3bdf
Juan Malaver <96750905+JuanMalaver1511@users.noreply.github.com>
2024-08-12 09:02:04 -0500

    Add files via upload

---
+++ precache-manifest.68687f2cb2feb1af21a11cc8c0a81069.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d11b043dd1393cb27f131266687f298b",
+    "url": "@yield('content-viewer-url')/index.html"
+  },
+  {
+    "revision": "70d46408e73087b42a5e",
+    "url": "@yield('content-viewer-url')/static/css/2.37be29a2.chunk.css"
+  },
+  {
+    "revision": "3b2e04116a10efd1ea65",
+    "url": "@yield('content-viewer-url')/static/css/main.0be79e8c.chunk.css"
+  },
+  {
+    "revision": "70d46408e73087b42a5e",
+    "url": "@yield('content-viewer-url')/static/js/2.7d799a75.chunk.js"
+  },
+  {
+    "revision": "cfb333affc1e9e4e40ea7a1e949500b6",
+    "url": "@yield('content-viewer-url')/static/js/2.7d799a75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b2e04116a10efd1ea65",
+    "url": "@yield('content-viewer-url')/static/js/main.9f8a8e7f.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "@yield('content-viewer-url')/static/js/main.9f8a8e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33a12fe361989d2d462",
+    "url": "@yield('content-viewer-url')/static/js/runtime-main.eac69593.js"
+  },
+  {
+    "revision": "a754c5ed00d8f6cca2a96794a2beaddc",
+    "url": "@yield('content-viewer-url')/static/media/trophy.a754c5ed.svg"
+  }
+]);
